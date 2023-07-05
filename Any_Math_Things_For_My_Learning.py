aa81ee8273733f699971b088922ad8cb6dcaa6d4
Foraho <80978568+Foraho@users.noreply.github.com>
2023-07-05 21:49:05 +0300

    Update Any_Math_Things_For_My_Learning.py
    -- add Standard_Deviation
    -- add Fraction

--- Any_Math_Things_For_My_Learning.py
+++ Any_Math_Things_For_My_Learning.py
@@ -1,8 +1,6 @@
-def factorial(num):
-    number = 1
-    for i in range(1,num+1):
-        number*=i
-    return number
+# -*- coding: utf-8 -*-
+# import math
+
 
 '''
  k       n!
@@ -16,7 +14,7 @@ def Binomial_Coefficient(n,k):
     :param k: total number of combinations // общее кол-во сочетаний
     :return: the number of combinations of k elements in n // число сочетаний k элементов в n
     '''
-    return int((factorial(n)) / (factorial(k) * (factorial(n-k)))) # применяем формулу биноминального коофицента
+    return int((math.factorial(n)) / (math.factorial(k) * (math.factorial(n-k)))) # применяем формулу биноминального коофицента
 
 def Euclid_Method(a,b):
     """
@@ -63,3 +61,48 @@ def Get_Any_Area (lst): # https://youtu.be/vUCJWwGQEYI
     x[1] += lst[0][0] * lst[len(lst) - 1][1]
     x = list(sorted(x)) # сортируем значения по возрастанию
     return (x[1] - x[0]) / 2
+
+def Standard_Deviation(lst):
+    '''
+    eng:
+    :param lst: array with arbitrary number of elements
+    :return: Float standard deviation
+    for example: [5,10,15,20.00000,25] -> 7.0710678118654755
+    if the array is empty then the answer is False
+    -----------
+    rus:
+    :param lst: массив с произволным кол-во элементов int или float элементов
+    :return: Float среднее квадтратичное отклонение
+    for example: [5,10,15,20.00000,25] -> 7.0710678118654755
+    если массив пустой то ответ False
+    '''
+    if len(lst) == 0:
+        return False
+    answer= 0
+    avg = sum(lst) / len(lst)
+    for i in lst:
+        answer += (i - avg) ** 2
+    return (answer / len(lst)) ** 0.5
+
+def Fraction(After_The_Decimal_Point = 0,period = 0):
+    '''
+    for exapmle: (23,6) mean 0.236666666666... = 213/900; (17,69) mean 0.17696969696969... = 1752/9900
+    :param After_The_Decimal_Point: numerator
+    :param period: denominator
+    :return: fraction
+    coming soon:
+    - обработчик целых частей
+    - сокращение готовой дроби
+    - обрботчик ошибок
+    - рефакторинг
+    '''
+    try:
+        num1 = int(str(After_The_Decimal_Point)+str(period))
+        num2 = int((After_The_Decimal_Point))
+    except:
+        return False
+    len1 = len(str(num1))
+    len2 = len(str(num2))
+    if num2 == 0: len2 = 0
+    mount = 10**len1-10**len2
+    return f'{num1-num2}/{mount}'
